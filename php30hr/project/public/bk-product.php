6dc7582c6efb0ca0c1a17249ec79bfc6320d8a61
shen <solove5171@gmail.com>
2025-06-11 17:06:54 +0800

    feat(publish): 增加產品列表並從資料庫拉取資料
    -增加從資料庫提取資料並列出
    
    -增加點選編輯按鈕時跳轉至product-edit.php

---
+++ php30hr/project/public/bk-product.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../inc/twig.inc.php'; //載入twig 功能
+require_once __DIR__ . '/../inc/db.inc.php';//載入db 功能
+
+//主要PHP程式區
+//判斷後台是否有登入
+if (isset($_SESSION['backend_login_flag']) && $_SESSION['backend_login_flag'] == true) {
+    //如果有登入的話$_SESSION['backend_login_flag'] 為 true 
+} else {
+    header("location: login-v2.php?message=nologin");
+}
+//用來判斷要用什麼版型渲染的變數
+$mode="";
+if (isset($_GET['mode']) && $_GET['mode'] != ""){
+    $mode = $_GET['mode'];
+}
+
+switch($mode){
+    case 'edit':
+        $pid = $_GET['pid'];
+        $stmt = $pdo->prepare("select * from products where id = :pid");
+        $stmt->execute([":pid"=>$pid]);
+        $data['result'] = $stmt->fetch();
+        $tmplFile = "partials/backend/product-edit.twig";
+        break;
+    default:
+        $stmt = $pdo->prepare("select * from products limit 8");
+        $stmt->execute();
+        $data["results"] = [];
+        while($row = $stmt->fetch()){
+            $data["results"][] = $row;
+        }
+        $tmplFile = "partials/backend/product-list.twig";
+        break;
+}
+
+//組合要傳給模版的資料陣列
+$data['useracc'] = $_SESSION['backend_login_acc'];
+
+//呼叫twig渲染html碼
+echo $twig->render($tmplFile, $data);
