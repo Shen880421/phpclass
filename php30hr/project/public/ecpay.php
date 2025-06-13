b3491fa3ff268503e1bc4cb22abe9457ba85c49c
shen <solove5171@gmail.com>
2025-06-13 11:48:26 +0800

    feat(ecpay): 新增ecpat檔案

---
+++ php30hr/project/public/ecpay.php
@@ -0,0 +1,31 @@
+<?php
+
+use Ecpay\Sdk\Factories\Factory;
+use Ecpay\Sdk\Services\UrlService;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+$factory = new Factory([
+    'hashKey' => 'pwFHCqoQZGmho4w6',
+    'hashIv'  => 'EkRm7iFT261dpevs',
+]);
+$autoSubmitFormService = $factory->create('AutoSubmitFormWithCmvService');
+
+$input = [
+    'MerchantID'        => '3002607',
+    'MerchantTradeNo'   => 'ALO' . time(),
+    'MerchantTradeDate' => date('Y/m/d H:i:s'),
+    'PaymentType'       => 'aio',
+    'TotalAmount'       => 100,
+    'TradeDesc'         => UrlService::ecpayUrlEncode('交易描述範例'),
+    'ItemName'          => '範例商品一批 100 TWD x 1',
+    'ChoosePayment'     => 'Credit',
+    'EncryptType'       => 1,
+
+    // 請參考 example/Payment/GetCheckoutResponse.php 範例開發
+    'ReturnURL'         => 'https://rnlmb-61-66-147-97.a.free.pinggy.link/phpclass/php30hr/project/public/ecpay.php',
+    'OrderResultURL'    => 'https://rnlmb-61-66-147-97.a.free.pinggy.link/phpclass/php30hr/project/public/ecpay.php',
+];
+$action = 'https://payment-stage.ecpay.com.tw/Cashier/AioCheckOut/V5';
+
+echo $autoSubmitFormService->generate($input, $action);
