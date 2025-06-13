b1dd2d15075b3057f24378e4c5960766653c9955
shen <solove5171@gmail.com>
2025-06-13 11:05:17 +0800

    feat(ecpay/sdk): 用composer下載綠界科技sdk至vender資料夾內

---
+++ php30hr/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'fdaa8f31d2c89861addd1cbec8066143c2bd8ea5',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'fdaa8f31d2c89861addd1cbec8066143c2bd8ea5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'ecpay/sdk' => array(
+            'pretty_version' => '1.3.2411050',
+            'version' => '1.3.2411050.0',
+            'reference' => '00f4d194d1577e8b35e85b85a951df3cd4a76f4a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../ecpay/sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
